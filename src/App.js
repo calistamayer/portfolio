d3e886f371c933014be5de3de41af0f9e42fd1d7
Calista Mayer <calista.a.mayer@gmail.com>
2021-02-16 12:11:53 -0800

    Navigation component skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import './index.css';
 import About from './components/About';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
