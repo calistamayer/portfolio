3d63626f39d7ef3393cf4bb5ef2176e07c9cc421
Calista Mayer <calista.a.mayer@gmail.com>
2021-02-16 20:36:19 -0800

    create nav component

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 function Navigation() {
-
     return (
-        <header>
-
-        </header>
+        <div>
+            <h2>
+                <a href="/">
+                    Calista Mayer
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li>
+                        About
+                    </li>
+                    <li>
+                        Portfolio
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                    <li>
+                        Resume
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 }
 
