d6352d48cadc625dfaed48e3b0ce0f9482f991bb
Calista Mayer <calista.a.mayer@gmail.com>
2021-02-21 20:38:03 -0800

    nav mapping

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-function Navigation() {
+function Navigation(props) {
+    const {
+        sections = [],
+        setCurrentSection,
+        currentSection
+    } = props;
+
     return (
         <div>
             <h2>
@@ -10,6 +16,18 @@ function Navigation() {
             </h2>
             <nav>
                 <ul className="flex-row">
+                    {sections.map((section) => (
+                        <li classname={`${currentSection.title === section.title && 'navActive'
+                            }`} key={section.name}>
+                                <span
+                                    onClick={() => {
+                                        setCurrentSection(section)
+                                    }}
+                                >
+                                    {section.title}
+                                </span>
+                            </li>
+                    ))}
                     <li>
                         About
                     </li>
